cb3fd684030d6ac467a78b814bdf7b6aec0e4b8f
Nikitha Lingapnor <nikithalingapnor@gmail.com>
2026-02-27 15:45:58 +0530

    Add workspace and user models, update imports

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,12 +6,20 @@ load_dotenv()
 app = FastAPI()
 
 from database import Base, engine
-from models.paper import Paper   # ✅ FIXED
+from models.paper import Paper
+from models.workspace import Workspace
+from models.user import User
 
 Base.metadata.create_all(bind=engine)
 
+# Include Routers
+from routers import auth
 from routers import papers
+from routers import workspace
+
+app.include_router(auth.router)
 app.include_router(papers.router)
+app.include_router(workspace.router)
 
 @app.get("/")
 def root():
